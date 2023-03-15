b0ae998ac5e39d7e43e0c87b132b105addbdb252
Justas <91596415+KrittyKrat@users.noreply.github.com>
2023-03-15 13:21:55 +0200

    small fix

--- outUtil.py
+++ outUtil.py
@@ -33,10 +33,19 @@ def uploadToFTP(fileName):
     fileDir = "Results/" + fileName
     fileEndDir = "/home/akademija/ftp/" + fileName
 
-    transport = paramiko.Transport((host, port))
-    transport.connect(None, user, pasw)
-    sftp = paramiko.SFTPClient.from_transport(transport)
-    sftp.put(fileDir, fileEndDir)
+    try:
+        transport = paramiko.Transport((host, port))
+        transport.connect(None, user, pasw)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+    except:
+        print("Failed to connect to the FTP server")
+        quit()
+
+    try:
+        sftp.put(fileDir, fileEndDir)
+    except:
+        print("Failed to upload file to FTP server")
+        quit()
 
     if sftp: sftp.close()
     if transport: transport.close()
